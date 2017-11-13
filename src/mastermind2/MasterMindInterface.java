f8b9b6f1b2ced05932cb21475c580c2bd56c9a91
ssharm02 <sarthak.sharma02@gmail.com>
2017-11-13 18:48:58 -0500

    Functioning Mastermind Program

---
+++ src/mastermind2/MasterMindInterface.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermind2;
+
+/**
+ *
+ * @author sarth
+ */
+public interface MasterMindInterface {
+
+    public abstract void drawGame();
+
+    public abstract int [] newGame();
+
+    public abstract int [] generateCompNum();
+    
+    public abstract String checkValues(String [] userInput, int [] computerNum);
+
+   public abstract void updateGame(String [][] resultArray, String[] clueArray);
+}
