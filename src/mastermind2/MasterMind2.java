a1ef3025829e395021a39cebb3d4e8db0d1cb9b7
Sarthak <sarthak.sharma02@gmail.com>
2019-06-02 18:00:50 -0400

    minor update

--- src/mastermind2/MasterMind2.java
+++ src/mastermind2/MasterMind2.java
@@ -10,18 +10,21 @@ public class MasterMind2 {
     public static String[][] board = new String[10][4];
     public static int[] tempComp;
     public static String[] computerNum = new String[10];
+    public static int num_tries = 10;
+    public static int correct_code = 2222;
+    public static int guess_times = 4;
 
     public static void main(String[] args) {
         // Calling the MM class with all methods
         MM mastermind = new MM();
         // Initialize the board
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 4; j++) {
-                board[i][j] = "";
+        for (int tries = 0; tries < num_tries; tries++) {
+            for (int guess_num = 0; guess_num < guess_times; j++) {
+                board[tries][guess_num] = "";
             }
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < num_tries; i++) {
             computerNum[i] = "    ";
         }
         // Call the draw game method
@@ -35,7 +38,7 @@ public class MasterMind2 {
             }
             computerNum[i] = mastermind.checkValues(board[i], tempComp);
 
-            if (Integer.parseInt(computerNum[i]) == 2222) {
+            if (Integer.parseInt(computerNum[i]) == correct_code) {
                 mastermind.finalUpdateGame(board, computerNum, tempComp);
             } else {
                 mastermind.updateGame(board, computerNum);
